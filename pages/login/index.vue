bb3f1ce8774164e6f7a6f94227d26b107d99c6d2
PanWeiZhang <1104365373@qq.com>
2021-11-22 10:38:22 +0800

    初始化基本架构

---
+++ pages/login/index.vue
@@ -0,0 +1,56 @@
+<template>
+	<view class="content">
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	import { test } from '../../api/dashboard.js'
+	export default {
+		data() {
+			return {
+				title: '登录'
+			}
+		},
+		onLoad() {
+			test().then((res)=>{
+				console.log("Success");
+			})
+		},
+		methods: {
+			bindClick(){
+				console.log("你好");
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
